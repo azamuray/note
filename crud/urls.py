a1f746abb6956d659d667ef6f0fe7bbf78eb6df9
ai-rage <azamat.murdalov@mail.ru>
2019-08-07 05:55:34 +0300

    add NoteDetailView

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import NoteListView
+from .views import NoteListView, NoteDetailView
 
+app_name = 'crud'
 urlpatterns = [
-	path('', NoteListView.as_view()),
+	path('', NoteListView.as_view(), name='note-list'),
+	path('<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (
+	ListView, DetailView
+)
 
 from .models import Note
 
 
 class NoteListView(ListView):
+	model = Note
+
+
+class NoteDetailView(DetailView):
 	model = Note
