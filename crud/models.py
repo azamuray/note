e977d7596ee7757f8d0114d80834636e068c8bc3
ai-rage <azamat.murdalov@mail.ru>
2019-08-07 04:46:20 +0300

    add Note model

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+	title 	= models.CharField("Название", max_length=120)
+	body 	= models.TextField("Описание")
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
